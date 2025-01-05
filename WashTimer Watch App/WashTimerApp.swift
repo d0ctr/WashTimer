8345a6d903130ac60417ae2d9522b5133d7035b9
Danila Vasilev <mr.den.vi@gmail.com>
2025-01-05 20:57:12 +0100

    Initial Commit

---
+++ WashTimer Watch App/WashTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashTimerApp.swift
+//  WashTimer Watch App
+//
+//  Created by Danila Vasilev on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WashTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WashTimer/WashTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashTimerApp.swift
+//  WashTimer
+//
+//  Created by Danila Vasilev on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WashTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
